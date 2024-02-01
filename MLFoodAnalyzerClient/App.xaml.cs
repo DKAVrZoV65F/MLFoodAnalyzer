453297f98ab5e1cb006f8eb30774f2a377dc205f
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2024-02-01 10:26:56 +0000

    Adding Colors, Fonts, Images, Icons, changing name application and adding localization.

--- MLFoodAnalyzerClient/App.xaml.cs
+++ MLFoodAnalyzerClient/App.xaml.cs
@@ -1,12 +1,24 @@
-﻿namespace MLFoodAnalyzerClient
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace MLFoodAnalyzerClient;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var windows = base.CreateWindow(activationState);
+
+        const int minWidth = 700;
+        const int minHeight = 870;
+
+        windows.MinimumWidth = minWidth;
+        windows.MinimumHeight = minHeight;
+
+        return windows;
     }
 }
--- MLFoodAnalyzerClient/AppShell.xaml.cs
+++ MLFoodAnalyzerClient/AppShell.xaml.cs
@@ -1,10 +1,27 @@
-﻿namespace MLFoodAnalyzerClient
+﻿using MauiApp1.Extension;
+using System.Globalization;
+
+namespace MLFoodAnalyzerClient;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public LocalizationResourceManager LocalizationResourceManager
+    => LocalizationResourceManager.Instance;
+    public AppShell()
     {
-        public AppShell()
+        InitializeComponent();
+
+        BindingContext = this;
+        AdminPanel.IsVisible = Preferences.Get("IsAdminPanel", false);
+
+        CultureInfo cultureInfo = new(Preferences.Get("LanguageApp", "ru-RU"));
+        LocalizationResourceManager.Instance.SetCulture(cultureInfo);
+
+        Application.Current.UserAppTheme = Preferences.Get("ThemeApp", "Default") switch
         {
-            InitializeComponent();
-        }
+            "Light" => AppTheme.Light,
+            "Dark" => AppTheme.Dark,
+            _ => AppTheme.Unspecified,
+        };
     }
 }
