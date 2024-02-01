b52d47baf6c47683803bfbf05be7e1056e15bcbe
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2024-02-01 10:31:30 +0000

    Adding Colors, Fonts, Images, Icons, change name application and add localization.

---
+++ MLFoodAnalyzerClient/Extension/Food.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp1.Pages;
+
+public class Food
+{
+    public Food()
+    {
+
+    }
+    public Food(string name, int age, string description)
+    {
+        Name = name;
+        Id = age;
+        Description = description;
+    }
+
+    public string Name { get; set; } = "";
+    public int Id { get; set; }
+    public string Description { get; set; } = "";
+}
