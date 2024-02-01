e172a122c1860091d8470d10598d62287088a048
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2024-02-01 12:19:36 +0000

    Fixing TCP Security.

--- MLFoodAnalyzerClient/Pages/MainPage.xaml.cs
+++ MLFoodAnalyzerClient/Pages/MainPage.xaml.cs
@@ -23,7 +23,7 @@ public partial class MainPage : ContentPage
     readonly int maxValue = 50;
 
     public MainPage()
-	{
+    {
         bool IsPolicyRead = Preferences.Get("IsPolicyRead", true);
         if (IsPolicyRead) GoToPolicy();
 
@@ -207,8 +207,17 @@ public partial class MainPage : ContentPage
             // добавляем в буфер
             response.Add((byte)bytesRead);
         }
-        var translation = Encoding.UTF8.GetString(response.ToArray());
-        textFromServer = translation + '\n';
+
+        if (!string.IsNullOrEmpty(Preferences.Get("SavedPasswordServer", "")))
+        {
+            string translation = Encoding.UTF8.GetString(response.ToArray());
+            textFromServer = (DecryptText(translation)) + '\n';
+        }
+        else
+        {
+            string translation = Encoding.UTF8.GetString(response.ToArray());
+            textFromServer = translation + '\n';
+        }
 
         response.Clear();
         networkStream.Close();
@@ -239,17 +248,10 @@ public partial class MainPage : ContentPage
         {
             text = EncryptText(text);
             await stream.WriteAsync(Encoding.UTF8.GetBytes($"1{text}\0"));
-            try
+            while ((bytesRead = stream.ReadByte()) != '\0')
             {
-                while ((bytesRead = stream.ReadByte()) != '\0')
-                {
-                    // добавляем в буфер
-                    response.Add((byte)bytesRead);
-                }
-            }
-            finally
-            {
-                textFromServer = "Error on server 2\n";
+                // добавляем в буфер
+                response.Add((byte)bytesRead);
             }
 
             string translation = Encoding.UTF8.GetString(response.ToArray());
@@ -282,55 +284,38 @@ public partial class MainPage : ContentPage
     private static string DecryptText(string CipherText)
     {
         byte[] toEncryptArray = Convert.FromBase64String(CipherText);
-        MD5CryptoServiceProvider objMD5CryptoService = new();
 
-        //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+        MD5CryptoServiceProvider objMD5CryptoService = new();
         byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(Preferences.Get("SavedPasswordServer", "")));
         objMD5CryptoService.Clear();
 
         using TripleDESCryptoServiceProvider objTripleDESCryptoService = new()
         {
-            //Assigning the Security key to the TripleDES Service Provider.
             Key = securityKeyArray,
-            //Mode of the Crypto service is Electronic Code Book.
             Mode = CipherMode.ECB,
-            //Padding Mode is PKCS7 if there is any extra byte is added.
             Padding = PaddingMode.PKCS7
         };
-
         var objCrytpoTransform = objTripleDESCryptoService.CreateDecryptor();
-        //Transform the bytes array to resultArray
         byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
         objTripleDESCryptoService.Clear();
-
-        //Convert and return the decrypted data/byte into string format.
         return UTF8Encoding.UTF8.GetString(resultArray);
     }
 
     private static string EncryptText(string PlainText)
     {
-        // Getting the bytes of Input String.
         byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
 
         MD5CryptoServiceProvider objMD5CryptoService = new();
-        //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
         byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(Preferences.Get("SavedPasswordServer", "")));
-        //De-allocatinng the memory after doing the Job.
         objMD5CryptoService.Clear();
 
         using TripleDESCryptoServiceProvider objTripleDESCryptoService = new()
         {
-            //Assigning the Security key to the TripleDES Service Provider.
             Key = securityKeyArray,
-            //Mode of the Crypto service is Electronic Code Book.
             Mode = CipherMode.ECB,
-            //Padding Mode is PKCS7 if there is any extra byte is added.
             Padding = PaddingMode.PKCS7
         };
-
-
         var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
-        //Transform the bytes array to resultArray
         byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
         objTripleDESCryptoService.Clear();
         return Convert.ToBase64String(resultArray, 0, resultArray.Length);
